84f634e0bbe00d47b789dde0f801cae33761f4a2
ops-boostwithlaunchpad <ops@boostwithlaunchpad.com>
2026-03-20 01:36:19 +0530

    fix(config): update supabase hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "dleptxeoeehmebdojfwj.supabase.co",
+        hostname: "ihljjqopdutekhxrhyal.supabase.co",
       }
     ],
   },
